ca28627c7c722c1a736bdc3a1f9fb0a81269caa5
shindou <sh.shindou@gmail.com>
2023-03-17 16:13:14 -0700

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Prints numbers between 00 to 99.
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -12,17 +13,18 @@ int main(void)
 	{
 		for (j = i + 1; j <= 99; j++)
 		{
-			putchar(i / 10 + '0');
-			putchar(i % 10 + '0');
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
 			putchar(' ');
-			putchar(j / 10 + '0');
-			putchar(j % 10 + '0');
-			if (i != 98 || j != 99)
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (i != 99 || j != 98)
 			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
+	putchar('\n');
 	return (0);
 }

f92a7c3af6e53d745127bc22c635bd6be60868ef
shindou <sh.shindou@gmail.com>
2023-03-27 07:55:30 -0700

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,9 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * reset_to_98 - check the code 
- *
- * Return: Always 0.
+ * reset_to_98 - check the code
+ *@n: input integer.
+ * Return: no return .
  */
 
 void reset_to_98(int *n)

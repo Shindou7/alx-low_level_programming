588490a500afb033ba7f0f0f8dd1b14af550e55d
shindou <sh.shindou@gmail.com>
2023-03-29 14:27:35 -0700

    9. Numbers have life; they re not just symbols on paper

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,21 +1,26 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
- * @n: the integer to print
+ * print_number - print numbers chars
+ * @m: integer params
+ * Return: 0
  */
+
 void print_number(int n)
 {
-	int n1;
+	unsigned int m;
+
+	m = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n1 = -n;
+		m = -n;
 	}
-	if (n1 / 10)
+
+	if (m / 10 != 0)
 	{
-		print_number(n1 / 10);
+		print_number(m / 10);
 	}
-	_putchar(n1 % 10 + '0');
+	_putchar((m % 10) + '0');
 }

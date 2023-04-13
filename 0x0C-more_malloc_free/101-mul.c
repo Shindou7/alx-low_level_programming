7a98a3a9b11f94a6fbbd00b0a9e68be6fe24848e
shindou <sh.shindou@gmail.com>
2023-04-13 15:03:26 -0700

    5. We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,95 +1,137 @@
-#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include <ctype.h>
 #include <string.h>
+#include <ctype.h>
+#include "main.h"
 /**
- * _checknum - determines length of the number and checks ifnumberis in base 10
- * @num: input number as string.
- * Return: length of the number.
+ * _is_zero - determines if any number is zero
+ * @argv: argument vector.
+ *
+ * Return: 1 if any number is zero, 0 otherwise.
  */
-int _checknum(char *num)
+int _is_zero(char *argv[])
 {
-	int len = strlen(num);
 	int i;
 
-	for (i = 0; i < len; i++)
+	for (i = 1; i <= 2; i++)
 	{
-		if (!isdigit(num[i]))
+		if (strlen(argv[i]) == 1 && argv[i][0] == '0')
+			return (1);
+	}
+	return (0);
+}
+
+/**
+ * _initialize_array - set memory to zero in a new array
+ * @ar: char array.
+ * @lar: length of the char array.
+ *
+ * Return: pointer of a char array.
+ */
+char *_initialize_array(char *ar, int lar)
+{
+	int i;
+
+	for (i = 0; i < lar; i++)
+		ar[i] = '0';
+	ar[lar] = '\0';
+	return (ar);
+}
+
+/**
+ * _checknum - determines length of the number
+ * and checks if number is in base 10.
+ * @argv: arguments vector.
+ * @n: row of the array.
+ *
+ * Return: length of the number.
+ */
+int _checknum(char *argv[], int n)
+{
+	int ln = strlen(argv[n]);
+	int i;
+
+	for (i = 0; i < ln; i++)
+	{
+		if (!isdigit(argv[n][i]))
 		{
 			printf("Error\n");
 			exit(98);
 		}
 	}
-	return (len);
+	return (ln);
 }
+
 /**
- * _reverse - reverse a string in place.
- * @str: input string to reverse.
- * Return: pointer to reversed string.
+ * _multiply - multiplies two numbers
+ * @a: first number as string.
+ * @b: second number as string.
+ *
+ * Return: pointer to result string.
  */
-char *_reverse(char *str)
+char *_multiply(char *a, char *b)
 {
-	int len = strlen(str);
-	int i;
+	int la = strlen(a);
+	int lb = strlen(b);
+	char *res = malloc(la + lb + 1);
+	int i, j, k, carry;
 
-	for (i = 0; i < len / 2; i++)
+	if (res == NULL)
+		return (NULL);
+
+	_initialize_array(res, la + lb);
+
+	for (i = la - 1; i >= 0; i--)
 	{
-		char temp = str[i];
+		carry = 0;
+		for (j = lb - 1, k = i + j + 1; j >= 0; j--, k--)
+		{
+			int tmp = (a[i] - '0') * (b[j] - '0') + carry + (res[k] - '0');
 
-		str[i] = str[len - 1 - i];
-		str[len - 1 - i] = temp;
+			res[k] = (tmp % 10) + '0';
+			carry = tmp / 10;
+		}
+		res[k] = carry + '0';
 	}
-	return (str);
+
+	while (*res == '0' && *(res + 1) != '\0')
+		res++;
+
+	return (res);
 }
+
 /**
- * main - Entry point. Program that multiplies two positive numbers.
+ * main - Entry point.
+ * program that multiplies two positive numbers.
  * @argc: number of arguments.
  * @argv: arguments vector.
+ *
  * Return: 0 - success.
  */
 int main(int argc, char *argv[])
 {
-	int i, j, len1, len2;
-	char *result;
+	char *a, *b, *res;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (98);
 	}
-	if (strcmp(argv[1], "0") == 0 || strcmp(argv[2], "0") == 0)
+
+	if (_is_zero(argv))
 	{
 		printf("0\n");
 		return (0);
 	}
-	len1 = _checknum(argv[1]);
-	len2 = _checknum(argv[2]);
-	result = (char *) calloc(len1 + len2 + 1, sizeof(char));
 
-	_reverse(argv[1]);
-	_reverse(argv[2]);
-	for (i = 0; i < len1; i++)
-	{
-		for (j = 0; j < len2; j++)
-		{
-			int product = (argv[1][i] - '0') * (argv[2][j] - '0');
-			int sum = result[i + j] + product;
+	a = argv[1];
+	b = argv[2];
 
-			result[i + j] = sum % 10;
-			result[i + j + 1] += sum / 10;
-		}
-	}
-	_reverse(result);
-	while (result[i] == 0)
-		i++;
-	if (i == len1 + len2)
-		i--;
-	for (; i < len1 + len2; i++)
-	{
-		printf("%d", result[i]);
-	}
-	printf("\n");
-	free(result);
+	res = _multiply(a, b);
+
+	printf("%s\n", res);
+
+	free(res);
+
 	return (0);
 }

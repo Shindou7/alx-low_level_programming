8039c1fc24768670a36c004a6875a3a71e781795
shindou <sh.shindou@gmail.com>
2023-04-13 09:59:39 -0700

    2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,12 +11,9 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	if (nmemb == 0 || size == 0)
-	{
 		return (NULL);
-	}
 
-	char *p = calloc(nmemb, size);
-	{
-		return (p);
-	}
+	void *p = calloc(nmemb, size);
+
+	return (p);
 }

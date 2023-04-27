bc552b10b2fba8bbd6559070527b564a333238ff
shindou <sh.shindou@gmail.com>
2023-04-27 06:29:54 -0700

    0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,7 +6,6 @@
  * @h: singly linked listss ,pointer to the head node of the list.
  *
  * Return: The number of nodes in the list.
- *
  */
 
 size_t print_list(const list_t *h)
@@ -16,7 +15,7 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)\n");
+			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%u] %s\n", h->len, h->str);
 

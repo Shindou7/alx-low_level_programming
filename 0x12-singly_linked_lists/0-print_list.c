63b51f11fe393f7db5fada6cf3a0d9d9adafc307
shindou <sh.shindou@gmail.com>
2023-04-27 06:37:26 -0700

    0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,20 +7,19 @@
  *
  * Return: The number of nodes in the list.
  */
-
 size_t print_list(const list_t *h)
 {
-	size_t numb_nodes = 0;
+	size_t numb_node;
 
+	numb_node = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-
 		h = h->next;
-		numb_nodes++;
+		numb_node++;
 	}
-	return (numb_nodes);
+	return (numb_node);
 }

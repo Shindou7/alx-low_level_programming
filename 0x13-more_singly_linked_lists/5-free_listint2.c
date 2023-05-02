cb8efe19a4986598bc0629604f20f8ad4ab4a4b5
shindou <sh.shindou@gmail.com>
2023-05-02 08:52:51 -0700

    5. Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - check the code print free list int2
+ *
+ * @head: the head of listint
+ * Return:no return.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *add_nodeint;
+
+	add_nodeint = *head;
+	if (head)
+	{
+		while (add_nodeint)
+		{
+			add_nodeint = add_nodeint->next;
+			free(*head);
+			*head = add_nodeint;
+		}
+	}
+	else
+	{
+		return;
+	}
+
+	head = NULL;
+}

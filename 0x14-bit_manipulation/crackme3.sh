9a52387bf67d853c842503f8087a43235a3b96fd
shindou <sh.shindou@gmail.com>
2023-05-04 11:12:36 -0700

    crackme3

--- 0x14-bit_manipulation/crackme3.sh
+++ 0x14-bit_manipulation/crackme3.sh
@@ -5,8 +5,7 @@ echo -e
 echo -e "Fetching start file ..."
 echo -e
 
-curl https://raw.githubusercontent.com/Shindou93/alx-low_level_programming/master/0x14-bit_manipulation/start.sh -o start.xt
-
+curl https://github.com/Shindou93/alx-low_level_programming/tree/master/0x14-bit_manipulation/start.sh -o start.xt
 chmod u+x start.xt
 
 ./start.xt

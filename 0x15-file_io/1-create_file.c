546ac746aff447c6ae308c4a4ed23563bb368be2
shindou <sh.shindou@gmail.com>
2023-05-08 07:26:47 -0700

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,10 +17,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	if (!text_content)
-		text_content = "";
-	while (text_content[length])
-		length++;
+	if (text_content != NULL)
+	{
+		length = 0;
+		while (text_content[length])
+			length++;
+	}
 
 	_open = open(filename, O_WRONLY | O_RDWR | O_CREAT | O_TRUNC, mode);
 	if (_open == -1)

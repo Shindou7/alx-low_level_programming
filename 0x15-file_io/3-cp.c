2c24ef5cf9b157546de37aaef78a8c5199fd94a0
shindou <sh.shindou@gmail.com>
2023-05-08 10:02:24 -0700

    3. cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -29,44 +29,45 @@ void close_the_file(int fd, char *file_to)
  * main - Copies the contents of a file to another file.
  * @argc: The number of argument.
  * @argv: pointers to the arguments.
- * Return: ........
+ * Return: 0 on success, otherwise exits with an error code.
  */
 int main(int argc, char *argv[])
 {
 	char buffer[1024];
 	ssize_t bytes_read, bytes_written;
-	int fd_from, fb_to;
-
-	fb_from = open(argv[1], O_RDONLY);
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	int fb_from, fd_to;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	if (fd_from == -1)
+	fb_from = open(argv[1], O_RDONLY);
+	if (fb_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close_the_file(fb_from, argv[1]);
 		exit(99);
 	}
-	while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
+	while ((bytes_read = read(fb_from, buffer, sizeof(buffer))) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
 		if (bytes_written != bytes_read)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close_the_file(fd_from, argv[1]);
+			close_the_file(fb_from, argv[1]);
 			close_the_file(fd_to, argv[2]);
 			exit(99);
 		}
 	}
-	close_the_file(fd_from, argv[1]);
+
+	close_the_file(fb_from, argv[1]);
 	close_the_file(fd_to, argv[2]);
 	return (0);
 }

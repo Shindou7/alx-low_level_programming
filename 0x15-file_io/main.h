1458cdca46120f02a7106e1c255322c4df56b5b6
shindou <sh.shindou@gmail.com>
2023-05-08 10:04:18 -0700

    main

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -18,5 +18,6 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
+void close_the_file(int fd, char *file_to);
+int main(int argc, char *argv[]);
 #endif

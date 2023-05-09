ab80c1acba0d0c3e0c88b26625b06ec8eb9615d3
shindou <sh.shindou@gmail.com>
2023-05-08 18:29:46 -0700

    100-elf_header

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -12,7 +12,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 void check_elf(unsigned char *pt);
 void _magic(unsigned char *pt);
@@ -27,14 +27,14 @@ void close_elf(int elf);
 
 /**
  * check_elf - check if it is an elf file.
- * @ptr: magic.
+ * @pt: magic.
  * Return: 1 if it is an elf file. 0 if not.
  */
 void check_elf(unsigned char *pt)
 {
 	unsigned char elf_magic[4] = { 0x7f, 'E', 'L', 'F' };
 
-	if (memcmp(e_ident, elf_magic, 4) != 0)
+	if (memcmp(pt, elf_magic, 4) != 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
 		exit(98);
@@ -43,18 +43,18 @@ void check_elf(unsigned char *pt)
 
 
 /**
- * print_magic - Prints the magic numbers of an ELF header.
- * @magic: A pointer to an array containing the ELF magic numbers.
+ * _magic - Prints the magic numbers of an ELF header.
+ * @pt: A pointer to an array containing the ELF magic numbers.
  * Description: Magic numbers are separated by spaces.
 */
-void print_magic(unsigned char *magic)
+void _magic(unsigned char *pt)
 {
 	int i;
 
 	printf(" Magic: ");
-	for (i = 0; i < EI_NIDENT; i++, magic++)
+	for (i = 0; i < EI_NIDENT; i++, pt++)
 	{
-		printf("%02x", *magic);
+		printf("%02x", *pt);
 		if (i == EI_NIDENT - 1)
 			printf("\n");
 		else
@@ -62,6 +62,22 @@ void print_magic(unsigned char *magic)
 	}
 }
 
+/**
+ * _class - Prints the class of an ELF header.
+ * @pt: A pointer to an array containing the ELF class.
+ */
+void _class(unsigned char *pt)
+{
+	printf("  Class:                             ");
 
+	unsigned int class = pt[EI_CLASS];
 
-
+	if (class == ELFCLASSNONE)
+		printf("none\n");
+	else if (class == ELFCLASS32)
+		printf("ELF32\n");
+	else if (class == ELFCLASS64)
+		printf("ELF64\n");
+	else
+		printf("<unknown: %x>\n", class);
+}

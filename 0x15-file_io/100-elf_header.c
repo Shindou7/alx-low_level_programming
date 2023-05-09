d83aa3d935606038d7097ca9dc6c2858015e0b7a
Shindou00 <98887570+Shindou93@users.noreply.github.com>
2023-05-09 16:54:41 +0100

    Update 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -192,7 +192,7 @@ void print_class(unsigned char *ep_pointer)
  * check_elf - Checks if a file is an ELF file.
  * @ep_pointer: A pointer to an array containing the ELF magic numbers.
  *
- * Description: If the file is not an ELF file - exit code 98.
+ * Return: If the file.
  */
 int check_elf(unsigned char *ep_pointer)
 {

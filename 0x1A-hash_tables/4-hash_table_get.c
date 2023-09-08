ef15b2cd1f1f40f0a06c121fc1e59398831e7812
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-09-08 21:52:39 +0100

    Create 4-hash_table_get.c

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - value associated with a key
+ *
+ * @ht: pointer to the hash
+ * @key: key hash
+ * Return: value hash.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+    unsigned long int index;
+    hash_node_t *current_node;
+
+    if (ht == NULL)
+        return (NULL);
+
+    if (key == NULL || *key == '\0')
+        return (NULL);
+
+    index = key_index((unsigned char *)key, ht->size);
+
+    current_node = ht->array[index];
+
+    while (current_node != NULL)
+    {
+        if (strcmp(current_node->key, key) == 0)
+            return (current_node->value);
+        current_node = current_node->next;
+    }
+
+    return (NULL);
+}

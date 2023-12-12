666ce67b51fa4fa63dddebd30e797f59861d96fe
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-12-12 16:14:06 +0100

    Update 106-linear_skip.c

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,44 @@
+#include "search_algos.h"
+
+/**
+ * linear_skip - searches for a value in a skip list
+ *
+ * @list: input list
+ * @value: value to search in
+ * Return: node with the target value, or NULL if not found
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *current;
+
+	if (list == NULL)
+		return (NULL);
+
+	current = list;
+
+	do {
+		list = current;
+		current = current->express;
+		printf("Checking index [%d] = [%d]\n", (int)current->index, current->n);
+	} while (current->express && current->n < value);
+
+	if (current->express == NULL)
+	{
+		list = current;
+		while (current->next)
+			current = current->next;
+	}
+
+	printf("Value found between indexes ");
+	printf("[%d] and [%d]\n", (int)list->index, (int)current->index);
+
+	while (list != current->next)
+	{
+		printf("Checking index [%d] = [%d]\n", (int)list->index, list->n);
+		if (list->n == value)
+			return (list);
+		list = list->next;
+	}
+
+	return (NULL);
+}

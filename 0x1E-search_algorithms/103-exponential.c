8e43da889dd849aeccdcd660934c8a2047a89715
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-12-12 16:39:42 +0100

    Update 103-exponential.c

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+
+/**
+ * exponential_search - searches for a value in an array
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
+int exponential_search(int *array, size_t size, int value)
+{
+
+}

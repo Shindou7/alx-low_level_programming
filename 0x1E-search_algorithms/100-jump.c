d7cd02cd2c527840da3fcf3d0375177ca22219e0
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-12-12 15:55:52 +0100

    Update 100-jump.c

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,42 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * jump_search - perform a jump search ie find square root,
+ * jump steps to find match, end linear search
+ * @array: given array of ints
+ * @size: size of array
+ * @value: value to search for
+ * Return: index at which value's found
+ */
+int jump_search(int *array, size_t size, int value)
+{
+    int idx, jump, step, prev_idx;
+
+    if (array == NULL || size == 0)
+        return (-1);
+
+    jump = (int)sqrt((double)size);
+    step = 0;
+    prev_idx = idx = 0;
+
+    do {
+        printf("Value checked array[%d] = [%d]\n", idx, array[idx]);
+
+        if (array[idx] == value)
+            return (idx);
+        step++;
+        prev_idx = idx;
+        idx = step * jump;
+    } while (idx < (int)size && array[idx] < value);
+
+    printf("Value found between indexes [%d] and [%d]\n", prev_idx, idx);
+
+    for (; prev_idx <= idx && prev_idx < (int)size; prev_idx++)
+    {
+        printf("Value checked array[%d] = [%d]\n", prev_idx, array[prev_idx]);
+        if (array[prev_idx] == value)
+            return (prev_idx);
+    }
+    return (-1);
+}
